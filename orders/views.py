818c329b85836597a754d58181474cd0a07d60a5
vanpipka <vanpipka@gmail.com>
2026-05-21 15:16:15 +0300

    2105

--- orders/views.py
+++ orders/views.py
@@ -438,12 +438,12 @@ def upload_product_image(request):
     if not product_id:
         return JsonResponse({'success': False, 'error': 'Missing product_id parameter'}, status=400)
     
-    #try:
-    #    img = ProductImage.objects.get(product_id = product_id)       
-    #    if img:
-    #        return JsonResponse({'success': True}, status=200)  
-    #except ProductImage.DoesNotExist:
-    #    ... 
+    try:
+        img = ProductImage.objects.get(product_id = product_id)       
+        if img:
+            return JsonResponse({'success': True}, status=200)  
+    except ProductImage.DoesNotExist:
+        ... 
     
     # удалим старые изображения
     # ProductImage.objects.filter(product_id = product_id).delete()
